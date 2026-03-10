8a3f4a09e779f3981d73a0fbbd07a3c4e0accf62
Tobias Plank <platob23@htl-kaindorf.at>
2026-03-10 14:49:26 +0100

    adding project feature finished.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
-import Image from "next/image";
-import {Button} from "@/components/ui/button";
-import {CirclePlus} from "lucide-react";
-import AppBarChart from "@/components/AppBarChart";
-
 export default function Home() {
-  return (
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-          <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-              <AppBarChart />
-          </div>
-          <div className="bg-primary-foreground p-4 rounded-lg"></div>
-          <div className="bg-primary-foreground p-4 rounded-lg"></div>
-          <div className="bg-primary-foreground p-4 rounded-lg"></div>
-          <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"></div>
-          <div className="bg-primary-foreground p-4 rounded-lg"></div>
-      </div>
-  );
+    return (
+        <div className="flex items-center justify-center h-[80vh]">
+            <h1 className="text-5xl font-semibold">Willkommen</h1>
+        </div>
+    )
 }
